4c4c12ff9e9cf97878d1cf63c528a66b754d40f4
root <kegonzalezca@est.utn.ac.cr>
2024-12-05 20:49:46 -0600

    examen

--- app/src/main/java/cr/ac/utn/appmovil/rooms/MainActivity.kt
+++ app/src/main/java/cr/ac/utn/appmovil/rooms/MainActivity.kt
@@ -1,12 +1,21 @@
 package cr.ac.utn.appmovil.rooms
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import cr.ac.utn.appmovil.rooms.util.util.Companion.openActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnApiCreateContact: Button = findViewById(R.id.ken_btnMainLogin)
+        btnApiCreateContact.setOnClickListener {
+            val intent = Intent(this, ken_LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
