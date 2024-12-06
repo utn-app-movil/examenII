a8b3fc3d32a400502537d26a5e2cfd21164b96a4
XxKeyDarkxX <kescalantem@est.utn.ac.cr>
2024-12-05 20:54:14 -0600

    Examen 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // OkHttp para manejo de solicitudes HTTP
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
@@ -74,4 +74,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
