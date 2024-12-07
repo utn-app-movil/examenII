659475781ec0b620504cf2c08feb3c406832178a
AlfredoSalazar27 <alfredosalazarale27@gmail.com>
2024-12-07 15:06:55 -0600

    examen 2

---
+++ app/src/main/java/adapter/Alf_RoomAdapter.kt
@@ -0,0 +1,4 @@
+package adapter
+
+class Alf_RoomAdapter {
+}
--- app/src/main/java/data/Alf_DBManager.kt
+++ app/src/main/java/data/Alf_DBManager.kt
@@ -1,37 +1,4 @@
-package cr.ac.utn.appmovil.data
+package data
 
-
-import android.content.Context
-import cr.ac.utn.appmovil.identities.Alf_Contact
-import cr.ac.utn.appmovil.identities.Alf_LoginEvent
-import cr.ac.utn.appmovil.interfaces.Alf_IDBManager
-import interfaces.APIService
-
-class Alf_DBManager(context: Context) : Alf_IDBManager {
-    private val dbHelper = alf_DBHelper(context)
-
-    override fun add(obj: Alf_Contact) {
-        dbHelper.addContact(obj)
-    }
-
-    override fun update(obj: Alf_Contact) {
-        dbHelper.modifyContact(obj)
-    }
-
-    override fun remove(id: String) {
-        dbHelper.removeContactById(id)
-    }
-
-    override fun getAll(): List<Alf_Contact> {
-        return dbHelper.fetchAllContacts()
-    }
-
-    override fun getById(id: String): Alf_Contact? {
-        return dbHelper.fetchContactById(id)
-    }
-
-    override fun getByFullName(fullName: String): Alf_Contact? {
-        return getAll().firstOrNull { "${it.Name} ${it.LastName}" == fullName }
-    }
-
+class Alf_DBManager {
 }
